8d92b0c38f451c0d4a0b04df7e93bdc14d999c95
kovach <goral@tutamail.com>
2023-08-15 15:26:43 -0400

    httrack

--- httrack/httrack.py
+++ httrack/httrack.py
@@ -3,24 +3,32 @@ import os
 import subprocess
 import time
 
-# loading the contents of a json file
-with open('/json/site/site.json', 'r') as file:
-    data = json.load(file)
-
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0"
-
-# iterating through the list of site URLs
-for url in data['urls']:
-    # path to the backup directory for the website
-    backup_path = '/mirror/site'
-
-    # getting the website URL
+json_file_path = '/json/site/site.json'
+wait_time = 6
+sleep_time = 6
+def run_backup(url, user_agent, backup_path):
     website = url
-
-    # create the backup directory if it doesn't exist
     os.makedirs(backup_path, exist_ok=True)
-
-    # Check if the backup already exists
-    if not os.path.exists(os.path.join(backup_path, website)):
-        # use httrack command to create a backup of the website
+    backup_directory = os.path.join(backup_path, website)
+    
+    if not os.path.exists(backup_directory):
         subprocess.run(['httrack', website, '--user-agent', user_agent, '-O', backup_path])
+
+while True:
+    if os.path.exists(json_file_path):
+        with open(json_file_path, 'r') as file:
+            data = json.load(file)
+            
+            if 'urls' in data and isinstance(data['urls'], list) and len(data['urls']) > 0:
+                user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0"
+                backup_path = '/mirror/site'
+                
+                for url in data['urls']:
+                    run_backup(url, user_agent, backup_path)
+                    
+        # Завершення роботи, чекання та перезапуск
+        print("Робота завершена. Чекання перед наступним запуском...")
+        time.sleep(sleep_time)
+    else:
+        print(f"Файл {json_file_path} не існує. Очікування {wait_time} секунд...")
+        time.sleep(wait_time)
